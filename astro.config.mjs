1ffb7626f4527ea465338e497099ce3ab100f7ef
sonvice <wvmc_es@hotmail.com>
2024-09-07 21:13:16 +0200

    Add adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  output:'server',
-
+  output: 'server',
+  adapter: netlify()
 });
